5c9d5a304c996da2bb0aaeb4f53cb89ab447f37e
sotoezequiel <ezequiel_soto_f4@hotmail.com>
2023-08-05 16:01:16 -0300

    fidex

--- src/componentes/Details.jsx
+++ src/componentes/Details.jsx
@@ -1,15 +1,16 @@
 import React from "react"
 import { useSelector } from "react-redux"
-import { useHistory } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import estilos from "../style/Details.module.css"
 import { useDispatch } from "react-redux"
 import { useEffect } from "react"
 import { GetCountries } from "../redux/actions"
 
 
-export function Details({ id }) {
+export default function Details() {
 
-
+    const {id} = useParams()
+    
     let stateRedux = useSelector(e => e)
     const dispatch = useDispatch()
     useEffect(() => {
@@ -18,21 +19,21 @@ export function Details({ id }) {
     }, [])
 
 
-    const history = useHistory()
+    const history = useNavigate()
     return (
         <>
             {stateRedux.allCountry ? stateRedux.allCountry.map(e => {
                 if (e.id == id) {
                     return (
                         <div className={estilos.todo}>
-                            <button onClick={() => history.push('/home')} className={estilos.volver}>Home</button>
+                            <button onClick={() => history('/home')} className={estilos.volver}>Home</button>
                             <div className={estilos.pais}>
                                 <div className={estilos.flag}>
                                     <img className={estilos.img} src={e.img} alt={"imagen de " + e.name} />
                                     <div className={estilos.flagR}>
-                                        <p className={estilos.nameP}>{e.name}</p>
+                                        <p className={estilos.name}>{e.name}</p>
                                         <p className={estilos.name}>Capital: {e.capital}</p>
-                                        <p className={estilos.name}>Population: {e.poblacion} people</p>
+                                        <p className={estilos.name}>Population: {e.poblacion}</p>
                                     </div>
                                 </div>
                                 <div className={estilos.extra}>
@@ -40,9 +41,10 @@ export function Details({ id }) {
                                         <p className={estilos.datos}>Continent:  { e.continente}</p>
                                         <p className={estilos.datos}>Subregion:  {e.subregion}</p>
                                         <p className={estilos.datos}>Area:  {e.area}km²</p>
-                                        <p className={estilos.datos}>{"Car side:    "+e.car}</p>
-                                        <p className={estilos.datos}>Main time zone:  {e.timezone}</p>
+                                        <p>{"Car side:    "+e.car}</p>
+                                        <p>Main time zone:  {e.timezone}</p>
                                     </div>
+                                    <iframe src={e.maps} width="500" height="400" ></iframe>
                                 </div>
                             </div>
                             <div>
